b5033e99745428fa50a358cb688f3abe2480f706
ffyoko <ffppy@msn.com>
2019-12-27 00:33:43 +0800

    Create timer.py

---
+++ timer.py
@@ -0,0 +1,10 @@
+from datetime import datetime
+
+def timer(start_time=None):
+    if not start_time:
+        start_time = datetime.now()
+        return start_time
+    elif start_time:
+        tmin, tsec = divmod((datetime.now() - start_time).total_seconds(), 60)
+        print(" Time taken: %i minutes and %s seconds." %
+              (tmin, round(tsec, 2)))
