b8bd149fe9ed56f3d66ff3b58dbbc104558c5ffa
ffyoko <ffppy@msn.com>
2020-08-16 01:13:41 +0800

    Update tree_parser.py

--- tree_parser.py
+++ tree_parser.py
@@ -26,25 +26,24 @@ def decision_paths(clf, feature_list, is_print=False):
             stack.append((children_right[node_id], parent_depth + 1))
         else:
             is_leaves[node_id] = True
-    
+
     if is_print:
         print("The binary tree structure has %s nodes and has "
-              "the following tree structure:"
-              % n_nodes)
+              "the following tree structure:" % n_nodes)
         for i in range(n_nodes):
             if is_leaves[i]:
                 print("%snode=%s leaf node." % (node_depth[i] * "\t", i))
             else:
                 print("%snode=%s test node: go to node %s if %s<=%s else to "
-                      "node %s."
-                      % (node_depth[i] * "\t",
-                         i,
-                         children_left[i],
-                         feature_list[feature[i]],
-                         round(threshold[i], 2),
-                         children_right[i],
-                         ))
-            
+                      "node %s." % (
+                          node_depth[i] * "\t",
+                          i,
+                          children_left[i],
+                          feature_list[feature[i]],
+                          round(threshold[i], 2),
+                          children_right[i],
+                      ))
+
     decision_path = dict({0: None})
     decision_depth = dict()
     for i in range(n_nodes):
@@ -52,10 +51,14 @@ def decision_paths(clf, feature_list, is_print=False):
         if not is_leaves[i]:
             left_node = children_left[i]
             right_node = children_right[i]
-            left_condition = (decision_path.get(i)+' and ' if decision_path.get(
-                i) else '')+feature_list[feature[i]]+'<='+str(round(threshold[i], 2))
-            right_condition = (decision_path.get(i)+' and ' if decision_path.get(
-                i) else '')+feature_list[feature[i]]+'>'+str(round(threshold[i], 2))
+            left_condition = (decision_path.get(i) +
+                              ' and ' if decision_path.get(i) else
+                              '') + feature_list[feature[i]] + '<=' + str(
+                                  round(threshold[i], 2))
+            right_condition = (decision_path.get(i) +
+                               ' and ' if decision_path.get(i) else
+                               '') + feature_list[feature[i]] + '>' + str(
+                                   round(threshold[i], 2))
             decision_path.update({left_node: left_condition})
             decision_path.update({right_node: right_condition})
 
@@ -80,26 +83,43 @@ def leaf_batches(clf, x, y):
 
     leaf_idx = np.array(range(n_nodes))[is_leaves]
 
-    batch_overdue = [(y[clf.apply(x) == i] > 0).sum()/len(y[clf.apply(x) == i])
-                     if len(y[clf.apply(x) == i]) > 0 else np.nan for i in leaf_idx]
+    batch_overdue = [
+        (y[clf.apply(x) == i] > 0).sum() /
+        len(y[clf.apply(x) == i]) if len(y[clf.apply(x) == i]) > 0 else np.nan
+        for i in leaf_idx
+    ]
     batch_num = [len(y[clf.apply(x) == i]) for i in leaf_idx]
-    proportion = [len(y[clf.apply(x) == i])/len(x)if len(x)
-                  > 0 else np.nan for i in leaf_idx]
-    overdue_num = [(y[clf.apply(x) == i] > 0).sum()for i in leaf_idx]
-    leaf_overdue = pd.DataFrame([leaf_idx, batch_num, proportion, overdue_num, batch_overdue], index=[
-                                'leaf_index', 'leaf_num', 'proportion', 'overdue_num', 'leaf_overdue']).T
+    proportion = [
+        len(y[clf.apply(x) == i]) / len(x) if len(x) > 0 else np.nan
+        for i in leaf_idx
+    ]
+    overdue_num = [(y[clf.apply(x) == i] > 0).sum() for i in leaf_idx]
+    leaf_overdue = pd.DataFrame(
+        [leaf_idx, batch_num, proportion, overdue_num, batch_overdue],
+        index=[
+            'leaf_index', 'leaf_num', 'proportion', 'overdue_num',
+            'leaf_overdue'
+        ]).T
     leaf_overdue.sort_values(by='leaf_overdue', ascending=False, inplace=True)
-    leaf_overdue[['leaf_index', 'leaf_num', 'overdue_num']] = leaf_overdue[[
-        'leaf_index', 'leaf_num', 'overdue_num']].astype(int)
-    leaf_overdue = leaf_overdue.append({'leaf_index': 'TOTAL', 'leaf_num': len(
-        y), 'proportion': 1, 'overdue_num': y.sum(), 'leaf_overdue': y.sum()/len(y)}, ignore_index=True)
-    leaf_overdue['lift'] = leaf_overdue['leaf_overdue'] / (y.sum()/len(y))
+    leaf_overdue[['leaf_index', 'leaf_num', 'overdue_num'
+                  ]] = leaf_overdue[['leaf_index', 'leaf_num',
+                                     'overdue_num']].astype(int)
+    leaf_overdue = leaf_overdue.append(
+        {
+            'leaf_index': 'TOTAL',
+            'leaf_num': len(y),
+            'proportion': 1,
+            'overdue_num': y.sum(),
+            'leaf_overdue': y.sum() / len(y)
+        },
+        ignore_index=True)
+    leaf_overdue['lift'] = leaf_overdue['leaf_overdue'] / (y.sum() / len(y))
     return leaf_overdue
 
 
 def index_generator(length, step):
-    body = np.arange(1, length//step+1).repeat(step)
-    tail = np.array(length//step).repeat(length % step)
+    body = np.arange(1, length // step + 1).repeat(step)
+    tail = np.array(length // step).repeat(length % step)
 
     return np.append(body, tail)
 
@@ -112,10 +132,12 @@ def rule_reformer(rule, splitor_sup=False):
         for j in i.split(' and '):
             f, _ = re.split(splitor, j)
             f = f.strip()
-            feature_dict.update({f: 'x["'+f+'"]'})
+            feature_dict.update({f: 'x["' + f + '"]'})
     pattern = re.compile('|'.join(feature_dict.keys()))
     feature_list = [
-        '('+pattern.sub(lambda x: feature_dict[x.group(0)], i)+')' for i in rule]
+        '(' + pattern.sub(lambda x: feature_dict[x.group(0)], i) + ')'
+        for i in rule
+    ]
 
     return ' or '.join(feature_list)
 
@@ -128,15 +150,21 @@ def rule_simulator(df, y, index, rule, splitor_sup=False):
     df['full'] = 1
     kwargs = {'rule': rule_reformer(rule, splitor_sup=splitor_sup)}
     df['hit'] = df.apply(decision_making_engine, **kwargs, axis=1)
-    df[f'hit_{y}'] = df.apply(lambda x: 1 if (
-        x['hit'] == 1 and x[f'{y}'] == 1) else 0, axis=1)
+    df[f'hit_{y}'] = df.apply(lambda x: 1
+                              if (x['hit'] == 1 and x[f'{y}'] == 1) else 0,
+                              axis=1)
     result = df.groupby(index)[['full', y, 'hit', f'hit_{y}']].sum()
-    result.rename(columns={'full': 'full_num', y: f'{y}_num',
-                           'hit': 'hit_num', f'hit_{y}': f'hit_{y}_num'}, inplace=True)
-    result[y] = result[f'{y}_num']/result['full_num']
-    result['hit'] = result['hit_num']/result['full_num']
-    result[f'hit_{y}'] = result[f'hit_{y}_num']/result['hit_num']
-    result['lift'] = result[f'hit_{y}']/result[f'{y}']
+    result.rename(columns={
+        'full': 'full_num',
+        y: f'{y}_num',
+        'hit': 'hit_num',
+        f'hit_{y}': f'hit_{y}_num'
+    },
+                  inplace=True)
+    result[y] = result[f'{y}_num'] / result['full_num']
+    result['hit'] = result['hit_num'] / result['full_num']
+    result[f'hit_{y}'] = result[f'hit_{y}_num'] / result['hit_num']
+    result['lift'] = result[f'hit_{y}'] / result[f'{y}']
 
     return result
 
@@ -286,62 +314,80 @@ def trees_to_dataframe(clf):
                         ' and ' + right_direct_value + ')'
                 decision_paths.update({left_direct_key: left_direct_value})
                 decision_paths.update({right_direct_key: right_direct_value})
-    ids = [str(t_id) + '-' + str(n_id)
-           for t_id, n_id in zip(tree_ids, node_ids)]
+    ids = [
+        str(t_id) + '-' + str(n_id) for t_id, n_id in zip(tree_ids, node_ids)
+    ]
     import pandas as pd
-    df = pd.DataFrame({'Tree': tree_ids, 'Node': node_ids, 'ID': ids,
-                       'Feature': fids, 'Split': splits, 'Yes': y_directs,
-                       'No': n_directs, 'Missing': missings, 'Gain': gains,
-                       'Cover': covers})
-    df = df[['Tree', 'Node', 'ID', 'Feature', 'Split',
-             'Yes', 'No', 'Missing', 'Gain', 'Cover']]
+    df = pd.DataFrame({
+        'Tree': tree_ids,
+        'Node': node_ids,
+        'ID': ids,
+        'Feature': fids,
+        'Split': splits,
+        'Yes': y_directs,
+        'No': n_directs,
+        'Missing': missings,
+        'Gain': gains,
+        'Cover': covers
+    })
+    df = df[[
+        'Tree', 'Node', 'ID', 'Feature', 'Split', 'Yes', 'No', 'Missing',
+        'Gain', 'Cover'
+    ]]
     return df, decision_paths, scores
-        
+
 
 if __name__ == "__main__":
-    x, y = make_classification(
-        n_samples=1000, n_features=45, n_informative=12, n_redundant=7, random_state=1)
+    x, y = make_classification(n_samples=1000,
+                               n_features=45,
+                               n_informative=12,
+                               n_redundant=7,
+                               random_state=1)
     feature_list = [f'feature_{i}' for i in range(0, 45)]
     x = pd.DataFrame.from_records(x, columns=feature_list)
     y = pd.Series.from_array(y, name='label')
-    clf = DecisionTreeClassifier(criterion='gini', max_depth=4,
-                                 min_samples_leaf=0.01, splitter='best', random_state=1)
+    clf = DecisionTreeClassifier(criterion='gini',
+                                 max_depth=4,
+                                 min_samples_leaf=0.01,
+                                 splitter='best',
+                                 random_state=1)
     clf.fit(x, y)
     decision_path, decision_depth = decision_paths(clf, feature_list, True)
     leaf_batch = leaf_batches(clf, x, y)
     leaf_batch['decision_path'] = leaf_batch['leaf_index'].map(
         decision_path.get)
     leaf_batch
-#     kwargs = {'feature_names': feature_list, 'class_names': [
-#         '0', '1'], 'filled': True, 'rounded': True, 'special_characters': True}
-#     dot_data = export_graphviz(clf, out_file=None, **kwargs)
-#     export_graphviz(clf, out_file='tree.dot', **kwargs)
-#     # with open("tree.dot", 'w') as f:
-#     #     f = export_graphviz(clf, out_file=f)
-#     with open('tree.dot', 'r') as f:
-#         dot_data = f.read()
-#     graph = pydotplus.graph_from_dot_data(dot_data)
-#     graph.write_png('tree.png')
-#     img = Image(graph.create_png())
-#     display(img)
+    #     kwargs = {'feature_names': feature_list, 'class_names': [
+    #         '0', '1'], 'filled': True, 'rounded': True, 'special_characters': True}
+    #     dot_data = export_graphviz(clf, out_file=None, **kwargs)
+    #     export_graphviz(clf, out_file='tree.dot', **kwargs)
+    #     # with open("tree.dot", 'w') as f:
+    #     #     f = export_graphviz(clf, out_file=f)
+    #     with open('tree.dot', 'r') as f:
+    #         dot_data = f.read()
+    #     graph = pydotplus.graph_from_dot_data(dot_data)
+    #     graph.write_png('tree.png')
+    #     img = Image(graph.create_png())
+    #     display(img)
     df = pd.concat([x, y], axis=1)
     y = 'label'
     step = 300
     df['index'] = index_generator(len(df), step)
     index = ['index']
-    rule = ['feature_37<=3.15 and feature_15>-1.17 and feature_39<=-1.13 and feature_9>-2.36',
-            'feature_37<=3.15 and feature_15<=-1.17 and feature_21>2.51']
+    rule = [
+        'feature_37<=3.15 and feature_15>-1.17 and feature_39<=-1.13 and feature_9>-2.36',
+        'feature_37<=3.15 and feature_15<=-1.17 and feature_21>2.51'
+    ]
     rule_simulator(df, y, index, rule)
 
-
-#     params = {
-#         'base_score': np.mean(y),
-#         'eta': 0.1,
-#         'max_depth': 3,
-#         'gamma': 3,
-#         'objective': 'reg:linear',
-#         'eval_metric': 'mae'
-#     }
+    #     params = {
+    #         'base_score': np.mean(y),
+    #         'eta': 0.1,
+    #         'max_depth': 3,
+    #         'gamma': 3,
+    #         'objective': 'reg:linear',
+    #         'eval_metric': 'mae'
+    #     }
     params = {
         'base_score': np.mean(y),
         'eta': 0.1,
@@ -355,19 +401,34 @@ if __name__ == "__main__":
         'booster': 'gbtree',
         'eval_metric': 'auc'
     }
+    num_boost_round = 3
     clf = xgb.train(params=params,
                     dtrain=xgb.DMatrix(data=x, label=y),
-                    num_boost_round=3)
+                    num_boost_round=num_boost_round)
     # import os
     # os.environ['PATH'] += os.pathsep + 'C:\Program Files (x86)\Graphviz2.38/bin/'
     xgb.to_graphviz(booster=clf, num_trees=0)
     clf.get_dump()
     clf.trees_to_dataframe()
-    model_to_py(params['base_score'], clf, 'predictor.py')
-    
-    import predictor as clf
+    model_to_py(params['base_score'], clf, 'clf.py')
+    import clf
     clf.xgb_predict(x.loc[0])
-    
     df, decision_paths, scores = trees_to_dataframe(clf)
+
+    predictor = pd.DataFrame(
+        data=clf.predict(data=xgb.DMatrix(data=x), pred_leaf=True),
+        columns=[f'num_trees_{i}' for i in range(0, num_boost_round)])
+    for i in range(0, num_boost_round):
+        predictor[f'num_trees_{i}'] = predictor[f'num_trees_{i}'].map(
+            lambda x: str(i) + '-' + str(x))
+        predictor[f'decision_paths_{i}'] = predictor[f'num_trees_{i}'].map(
+            decision_paths)
+        predictor[f'scores_{i}'] = predictor[f'num_trees_{i}'].map(scores)
+    predictor['linear_predictor'] = predictor[[
+        f'scores_{i}' for i in range(0, num_boost_round)
+    ]].sum(axis=1)
+    predictor['predict_result_repeat'] = predictor['linear_predictor'].map(
+        lambda x: 1 / (1 + np.exp(-x)))
+    predictor['predict_result'] = clf.predict(data=xgb.DMatrix(data=x))
     
     
